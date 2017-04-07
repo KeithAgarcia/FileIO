e7d00c1ee2de1cc21c920d5d95766add53fee62b
Keith Garcia <Keith@Keiths-MacBook.local>
2017-04-07 17:20:06 -0400

    Update

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,12 +1,24 @@
 package com.theironyard.charlotte;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
-
-
 public class Main {
-    public static Scanner input = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        File f = new File("shirt.txt");
+        FileWriter fw = new FileWriter(f);
+
+        System.out.println("What brand of shirt do you want?");
+        String brand = scanner.nextLine();
+        fw.write(brand + "\n");
+        System.out.println("Select [men] or [women]");
+        String gender = scanner.nextLine();
+        fw.append(gender + "\n");
+
+        fw.close();
 
     }
 }
