f1170b4799976f0038ebd1f17d207283e58dd7ee
Keith Garcia <Keith@Keiths-MacBook.local>
2017-04-09 15:35:10 -0400

    Update

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,11 +3,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
 import java.util.Scanner;
+
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args)throws IOException {
+
         File f = new File("shirt.txt");
         FileWriter fw = new FileWriter(f);
 
@@ -17,8 +19,11 @@ public class Main {
         System.out.println("Select [men] or [women]");
         String gender = scanner.nextLine();
         fw.append(gender + "\n");
-
         fw.close();
 
+        scanner = new Scanner(f);
+        scanner.useDelimiter("\\Z");
+        String contents = scanner.next();
+        System.out.println(contents);
     }
 }
